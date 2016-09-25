b2b9f3b7046b53d20b36659a89d106109e354486
seeamkhan <zafi005@gmail.com>
2016-09-26 02:51:50 +0600

    added log files

--- test.py
+++ test.py
@@ -4,6 +4,10 @@ import itertools
 
 script, filename = argv
 
+video_string = "Hardware: System  Video"
+matrox_not_installed_string = "Matrox DSX.utils version is empty."
+matrox_failed_string = "MatroxFileWriter.vip: Load ok. Init failed."
+
 def count_plugin():
     success_count = 0
     fail_count = 0
@@ -11,6 +15,7 @@ def count_plugin():
     vip_string = ".vip: "
     success = "Init ok"
     fail = "Init failed"
+
     list = []
     failedPluginList=[]
     current=''
@@ -19,15 +24,28 @@ def count_plugin():
         if vip_string in line:
             if success in line:
                 current = i
-                success_count = success_count+1
+                success_count += 1
             if fail in line:
-                fail_count = fail_count+1
-                print '\n'.join(list[current+1:i])
+                fail_count += 1
+                # print '\n'.join(list[current+1:i])
 
     print "Successful plugin load: %d" % success_count
     print "Fail plguin: %d" % fail_count
+
+
+
+def final_fail_count():
+    f = open(filename)
+    data = f.read()
+    # print data
+    if video_string not in data:
+        print "vga found"
+    else:
+        print "video found"
+
+
             # print line
 
 
-
 count_plugin()
+final_fail_count()
---
+++ test2.py
@@ -0,0 +1,5 @@
+a = "this is test"
+b =  "nothings good"
+c = 'good test'
+if b not in c or a in b:
+    print "success"
