368ba5bf42109dd29378b13082c6de0378dbefaa
seeam khan <zafi005@gmail.com>
2016-09-26 14:07:25 +0600

    plugin count completed.

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 from sys import argv
 import itertools
 
-
 script, filename = argv
 
 video_string = "Hardware: System  Video"
 matrox_not_installed_string = "Matrox DSX.utils version is empty."
-matrox_failed_string = "MatroxFileWriter.vip: Load ok. Init failed."
+matrox_failed_string = "MatroxFileWriter.vip:"
+
 
 def count_plugin():
     success_count = 0
@@ -14,38 +14,45 @@ def count_plugin():
     f = open(filename)
     vip_string = ".vip: "
     success = "Init ok"
-    fail = "Init failed"
+    load_fail = ".vip: Load failed."
+    init_fail = "Init failed"
 
     list = []
-    failedPluginList=[]
-    current=''
+    failedPluginList = []
+    current = ''
     for i, line in enumerate(f):
         list.append(line)
         if vip_string in line:
             if success in line:
                 current = i
                 success_count += 1
-            if fail in line:
+            if load_fail in line or init_fail in line:
                 fail_count += 1
                 # print '\n'.join(list[current+1:i])
 
-    print "Successful plugin load: %d" % success_count
-    print "Fail plguin: %d" % fail_count
+    # print "Successful plugins load: %d" % success_count
+    # print ".......Fail plugins: %d" % fail_count
 
-
-
-def final_fail_count():
     f = open(filename)
     data = f.read()
     # print data
-    if video_string not in data:
-        print "vga found"
-    else:
+    if video_string in data:
         print "video found"
+        if (matrox_not_installed_string in data) and (matrox_failed_string in data):
+            print "matrox failed found."
+            fail_count -= 1
+            # print fail_count
+    else:
+        print "VGA found"
+        if matrox_failed_string in data:
+            print "matrox failed found."
+            fail_count -= 1
+            # print fail_count
 
 
-            # print line
+    print "Successful plugins load: %d" % success_count
+    print "Fail plugins: %d" % fail_count
+
 
 
 count_plugin()
-final_fail_count()
